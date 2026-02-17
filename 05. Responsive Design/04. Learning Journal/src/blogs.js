7aade6411d8eddc30e68112339cc7af311fcee11
James Deogrades <james.deogrades@gmail.com>
2026-02-16 19:26:17 -0500

    ADD | Add Learning Journal

---
+++ 05. Responsive Design/04. Learning Journal/src/blogs.js
@@ -0,0 +1,79 @@
+export default [
+    {
+        title: "Blog one",
+        date: "JULY 23, 2022",
+        description:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        id: "blog-one",
+        href: "blog-one.html",
+        imagePath: "images/blog-image-01.webp",
+        imageDimensions: [2070, 1380],
+        alt: "A monitor displaying a code editor with JavaScript next to a browser window showing the React logo and default starter page.",
+    },
+    {
+        title: "Blog two",
+        date: "JULY 23, 2022",
+        description:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        id: "blog-two",
+        href: "blog-two.html",
+        imagePath: "images/blog-image-02.webp",
+        imageDimensions: [1055, 1346],
+        alt: "A laptop sitting on a table showing a site with the text 'I design and develop experiences that make people's lives simple'.",
+    },
+    {
+        title: "Blog three",
+        date: "JULY 23, 2022",
+        description:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        id: "blog-three",
+        href: "blog-three.html",
+        imagePath: "images/blog-image-03.webp",
+        imageDimensions: [2070, 1380],
+        alt: "A woman with long blonde hair spinning on a sandy beach during sunset, wearing a dark sweater and light-colored jeans.",
+    },
+    {
+        title: "Blog four",
+        date: "JULY 23, 2022",
+        description:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        id: "blog-four",
+        href: "blog-four.html",
+        imagePath: "images/blog-image-04.webp",
+        imageDimensions: [1064, 1330],
+        alt: "A hand holding a clear, incandescent light bulb against a gradient background of soft blue and pink.",
+    },
+    {
+        title: "Blog five",
+        date: "JULY 23, 2022",
+        description:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        id: "blog-five",
+        href: "blog-five.html",
+        imagePath: "images/blog-image-05.webp",
+        imageDimensions: [2070, 1380],
+        alt: "An abstract composition of vibrant purple and pink hues featuring speckled patterns and a diagonal beam of light.",
+    },
+    {
+        title: "Blog six",
+        date: "JULY 23, 2022",
+        description:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        id: "blog-six",
+        href: "blog-six.html",
+        imagePath: "images/blog-image-06.webp",
+        imageDimensions: [2231, 1255],
+        alt: "A MacBook Pro displaying HTML code on a desk next to a small green succulent plant in a textured glass pot.",
+    },
+    {
+        title: "Blog seven",
+        date: "JULY 23, 2022",
+        description:
+            "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        id: "blog-seven",
+        href: "blog-seven.html",
+        imagePath: "images/blog-image-07.webp",
+        imageDimensions: [1035, 1380],
+        alt: "A laptop with a code editor on a desk under a ring lamp, with a city sunset visible through the window.",
+    },
+];
---
+++ 05. Responsive Design/04. Learning Journal/src/index.js
@@ -0,0 +1,66 @@
+import blogs from "./blogs.js";
+
+class LearningJournal {
+    static blogsGrid = document.getElementById("blogs-grid");
+    static blogsLoaded = 0;
+
+    static init() {
+        if (window.location.pathname.endsWith("index.html")) {
+            this.loadBlogs(6);
+        } else {
+            this.loadBlogs(3, true);
+        }
+
+        document.addEventListener("click", this.onClick.bind(this));
+    }
+
+    /**
+     * @param {MouseEvent} event
+     */
+    static onClick(event) {
+        const { target } = event;
+
+        if (target.id === "view-more") {
+            this.loadBlogs(6);
+        }
+    }
+
+    static loadBlogs(blogLoadCount, reverse = false) {
+        const loadStop = this.blogsLoaded + blogLoadCount;
+
+        for (
+            this.blogsLoaded;
+            this.blogsLoaded < Math.min(loadStop, blogs.length);
+            this.blogsLoaded++
+        ) {
+            const blogIndex = reverse
+                ? blogs.length - this.blogsLoaded - 1
+                : this.blogsLoaded;
+
+            this.blogsGrid.innerHTML += this.generateBlog(
+                blogs[blogIndex],
+            );
+        }
+
+        if (loadStop > blogs.length) {
+            document.getElementById("view-more")?.remove();
+        }
+    }
+
+    static generateBlog(blogData) {
+        return `
+        <article id="${blogData.id}" class="blog">
+            <a href="${blogData.href}">
+                <img class="blog-image" src="${blogData.imagePath}"
+                    alt="${blogData.alt}" width="${blogData.imageDimensions[0]}"
+                    height="${blogData.imageDimensions[1]}">
+                <h3 class="blog-title">${blogData.title}</h3>
+                <p class="blog-date">${blogData.date}</p>
+                <p class="blog-description">${blogData.description}</p>
+            </a>
+        </article>
+        `;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => LearningJournal.init());
